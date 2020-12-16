f25a9a6db409c44ea304033add61b02bc944993a
fadilparves <fadil.parves@gmail.com>
2020-12-16 22:44:50 +0800

    adding link retrieval code

--- scraper.py
+++ scraper.py
@@ -3,9 +3,14 @@ from selenium import webdriver as wb
 import pandas as pd
 import time
 from selenium.webdriver.chrome.options import Options
+import time
 
 options = Options()
-options.binary_location = "/usr/bin/google-chrome"    #chrome binary location specified here
-options.add_argument("--start-maximized") 
+options.binary_location = "/usr/bin/google-chrome"
 wb = wb.Chrome('/usr/lib/chromedriver', options=options)
-wb.get('https://shopee.com.my/-FREE-GIFTS-A6-Thermal-Sticker-Thermal-Paper-Shopee-Waybill-Shipping-Label-Consignment-Note-Sticker-100*150mm-10*15cm-i.17814982.6229533411')
+wb.get('https://shopee.com.my/Computer-Accessories-cat.174')
+wb.find_element_by_xpath("//button[contains(string(), 'English')]").click()
+
+
+
+
