6a09b8faef96ce4b83b290e6845d2ed6c0ea2fa3
fadilparves <fadil.parves@gmail.com>
2020-12-17 19:45:23 +0800

    load dynamic content

--- scraper.py
+++ scraper.py
@@ -10,9 +10,25 @@ options.binary_location = "/usr/bin/google-chrome"
 wb = wb.Chrome('/usr/lib/chromedriver', options=options)
 wb.get('https://shopee.com.my/Computer-Accessories-cat.174')
 wb.find_element_by_xpath("//button[contains(string(), 'English')]").click()
-productsList = wb.find_elements_by_class_name('col-xs-2-4.shopee-search-item-result__item')
-test_href = productsList[0].find_element_by_tag_name('a')
-print(test_href.get_property('href'))
 
+links = []
+condition = True
+
+while condition:
+    time.sleep(5)
+    wb.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(3)
+    productsList = wb.find_elements_by_class_name('col-xs-2-4.shopee-search-item-result__item')
+    for i in range(len(productsList)):
+        print(i)
+        href_finder = productsList[i].find_element_by_tag_name('a')
+        links.append(href_finder.get_property('href'))
+
+    condition=False
+
+    
+df = pd.DataFrame()
+df['links'] = links
+df.to_csv("test_links.csv")
 
 
